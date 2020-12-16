ef83d881bafa9028d49b5b342b596103d5373b17
Trainee 18 <sagar.vartak@contentstack.com>
2020-12-16 19:39:05 +0530

    changed controllers/.js for hosting.8
    added response on page

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -5,13 +5,13 @@ const getAlltasks = async (req, res) => {
   let alltasks = await Task.find();
   console.log("alll tasks are:", alltasks);
   //res.render(users);
-  res.send("tasks fetched");
+  res.send("tasks fetched", alltasks);
 };
 const addTask = async (req, res) => {
   try {
     let adTask = await Task.insertMany({ taskName: "NEW TASK" });
     console.log(adTask);
-    res.send("ADDED task sucessFully");
+    res.send("ADDED task sucessFully", adTask);
   } catch (err) {
     console.log(err);
   }
@@ -19,14 +19,14 @@ const addTask = async (req, res) => {
 const deleteTask = async (req, res) => {
   let delTask = await Task.deleteOne().select({ taskName: "Task name 1" });
   console.log(delTask);
-  res.send("deleted the Task");
+  res.send("deleted the Task", delTask);
 };
 const updateTask = async (req, res) => {
   let uptask = await Task.find({ taskName: "Task name 2" }).updateOne({
     status: "completed",
   });
   console.log(uptask);
-  res.send("updated task");
+  res.send("updated task", upTask);
 };
 
 module.exports.getAlltasks = getAlltasks;
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,12 @@ const User = require("../models/userSchema");
 const getAllUsers = async (req, res) => {
   let users = await User.find().select("email");
   console.log(users);
-  res.send("users fetched");
+  res.send("users fetched", users);
 };
 const deleteUser = async (req, res) => {
   let users = await User.deleteOne().select("email");
   console.log(users);
-  res.send("deleted the user");
+  res.send("deleted the user", users);
 };
 
 module.exports.getAllUsers = getAllUsers;

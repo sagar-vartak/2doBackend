e70d30284fd334d53447104793917a662f9f81a1
Trainee 18 <sagar.vartak@contentstack.com>
2020-12-16 19:48:34 +0530

    changed controllers/.js for hosting.9
    added response on page

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -5,7 +5,7 @@ const getAlltasks = async (req, res) => {
   let alltasks = await Task.find();
   console.log("alll tasks are:", alltasks);
   //res.render(users);
-  res.send("tasks fetched", alltasks);
+  res.send(alltasks);
 };
 const addTask = async (req, res) => {
   try {
@@ -19,14 +19,14 @@ const addTask = async (req, res) => {
 const deleteTask = async (req, res) => {
   let delTask = await Task.deleteOne().select({ taskName: "Task name 1" });
   console.log(delTask);
-  res.send("deleted the Task", delTask);
+  res.send(delTask);
 };
 const updateTask = async (req, res) => {
   let uptask = await Task.find({ taskName: "Task name 2" }).updateOne({
     status: "completed",
   });
   console.log(uptask);
-  res.send("updated task", upTask);
+  res.send(upTask);
 };
 
 module.exports.getAlltasks = getAlltasks;
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,12 @@ const User = require("../models/userSchema");
 const getAllUsers = async (req, res) => {
   let users = await User.find().select("email");
   console.log(users);
-  res.send("users fetched", users);
+  res.send(users);
 };
 const deleteUser = async (req, res) => {
   let users = await User.deleteOne().select("email");
   console.log(users);
-  res.send("deleted the user", users);
+  res.send(users);
 };
 
 module.exports.getAllUsers = getAllUsers;
